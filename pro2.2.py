6ac55db561b88167774d231e7a2565c084858aba
vraj shingala <vrajshingala69@gmail.com>
2026-03-06 11:23:41 +0530

    Add files via upload

---
+++ pro2.2.py
@@ -0,0 +1,15 @@
+#2.2 Program to demonstrate user-defined exception
+
+class NegativeNumberError(Exception):
+    pass
+
+try:
+    num = int(input("Enter a positive number: "))
+    
+    if num < 0:
+        raise NegativeNumberError("Negative numbers are not allowed")
+    
+    print("You entered:", num)
+
+except NegativeNumberError as e:
+    print("User Defined Exception Occurred:", e)
